090c7f9a1937d33607efd800170df8870a1cb4a6
albertorm1 <albertorodriguezmuelas@outlook.com>
2024-10-16 12:58:12 +0200

    fix: missing files

---
+++ apuntes-dam/assets/js/data/swconf.js
@@ -0,0 +1,26 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1729076166',resources: [
+      '/apuntes-dam/assets/css/jekyll-theme-chirpy.css',
+      '/apuntes-dam/',
+      
+        '/apuntes-dam/categories/',
+      
+        '/apuntes-dam/accesoadatos/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
